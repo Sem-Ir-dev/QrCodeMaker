14bcf9fb74614fc1996ae546573515de2be9b5d8
Sem_Ir <semi.r04.sr@gmail.com>
2021-07-24 18:33:16 +0500

    Translated into English

--- QrCodeMaker.py
+++ QrCodeMaker.py
@@ -15,19 +15,19 @@ window.iconbitmap('scanner.ico')
 window.resizable(False, False)
 window['bg'] = '#868686'
 
-lb0 = Label(window, text='Введите текст для шифрования:', bg='#868686', font=('Arial', 12))
+lb0 = Label(window, text='Enter the text to encrypt:', bg='#868686', font=('Arial', 12))
 lb0.pack()
 
 tx_poly = Text(window, width=30, height=10, font='Arial', bg='#4C4C4C', fg='white', wrap=WORD)
 tx_poly.pack()
 
-lb1 = Label(window, text='Введите название файла:', bg='#868686', font=('Arial', 12))
+lb1 = Label(window, text='Enter file name:', bg='#868686', font=('Arial', 12))
 lb1.pack()
 
 name_poly = Entry(window, width=30, font='Arial', bg='#4C4C4C', fg='white')
 name_poly.pack()
 
-but = Button(window, text='Шифровать', width=10, height=2, bg='#999999', command=qcode)
+but = Button(window, text='Encrypt', width=10, height=2, bg='#999999', command=qcode)
 but.pack()
 
 window.mainloop()
