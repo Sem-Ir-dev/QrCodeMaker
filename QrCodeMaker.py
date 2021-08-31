9f869859887e7abcfc05d737d1efe9990db7c104
Sem_Ir <semi.r04.sr@gmail.com>
2021-08-31 18:23:14 +0500

    New design

--- QrCodeMaker.py
+++ QrCodeMaker.py
@@ -1,10 +1,9 @@
 import qrcode
 from tkinter import *
 
-bg_input = '#D3DEA4'
-bg_button = '#CBDE77'
-bg_back = '#E1E6E2'
-fg_color = '#2D2D2F'
+blue = '#8398E3'
+white = '#fff'
+black = '#000'
 
 
 def qcode():
@@ -19,21 +18,21 @@ window.title('QrCodeMaker')
 window.geometry('350x330+480+200')
 window.iconbitmap('scanner.ico')
 window.resizable(False, False)
-window['bg'] = bg_back
+window['bg'] = white
 
-lb0 = Label(window, text='Enter the text to encrypt:', fg=fg_color, bg=bg_back, font=('Arial', 12))
+lb0 = Label(window, text='Enter the text to encrypt:', fg=black, bg=white, font=('Arial', 12))
 lb0.pack()
 
-tx_poly = Text(window, width=30, bd=1, height=10, fg=fg_color, font='Arial', bg=bg_input, wrap=WORD)
+tx_poly = Text(window, width=30, bd=1, height=10, fg=black, font='Arial', bg=white, wrap=WORD)
 tx_poly.pack()
 
-lb1 = Label(window, text='Enter file name:', fg=fg_color, bg=bg_back, font=('Arial', 12))
+lb1 = Label(window, text='Enter file name:', fg=black, bg=white, font=('Arial', 12))
 lb1.pack()
 
-name_poly = Entry(window, width=30, bd=1, fg=fg_color, font='Arial', bg=bg_input)
+name_poly = Entry(window, width=30, bd=1, fg=black, font='Arial', bg=white)
 name_poly.pack()
 
-but = Button(window, text='Encrypt', bd=1, fg=fg_color, width=12, height=2, bg=bg_button, command=qcode)
+but = Button(window, text='Encrypt', bd=0, fg=white, width=12, height=2, bg=blue, command=qcode)
 but.place(x=132, y=270)
 
 window.mainloop()
